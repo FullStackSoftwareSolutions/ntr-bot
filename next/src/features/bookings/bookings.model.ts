bd7cad0b156849c6eac81045e21f1dc4116ef021
Josh Kay <josh.m.kay@gmail.com>
2024-06-22 01:55:48 +0200

    Fix for position type error

--- next/src/features/bookings/bookings.model.ts
+++ next/src/features/bookings/bookings.model.ts
@@ -84,5 +84,5 @@ export const getPlayersForBookingPosition = (
   position: Positions,
 ) =>
   booking.playersToBookings.filter(
-    (playerToBooking) => playerToBooking.position === position,
+    (playerToBooking) => playerToBooking.position === position.toString(),
   );
